74d175826b88edad46a598014bf3e35f404f2bbf
Legobuilder0813 <legobuilder0813@gmail.com>
2016-12-14 14:05:05 -0500

    Make /link available to everyone

--- src/main/java/com/palacemc/dashboard/commands/CommandLink.java
+++ src/main/java/com/palacemc/dashboard/commands/CommandLink.java
@@ -3,16 +3,11 @@ package com.palacemc.dashboard.commands;
 import com.palacemc.dashboard.Dashboard;
 import com.palacemc.dashboard.handlers.MagicCommand;
 import com.palacemc.dashboard.handlers.Player;
-import com.palacemc.dashboard.handlers.Rank;
 
 /**
  * Created by Marc on 12/12/16.
  */
 public class CommandLink extends MagicCommand {
-
-    public CommandLink() {
-        super(Rank.WIZARD);
-    }
 
     @Override
     public void execute(Player player, String label, String[] args) {
