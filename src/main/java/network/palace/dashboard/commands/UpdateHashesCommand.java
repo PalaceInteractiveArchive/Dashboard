572cc011078a087ccf05574346326c6511f270ad
Marc <legobuilder0813@gmail.com>
2019-02-16 11:06:01 -0500

    Update /updatehashes to support multiple versions of Resource Packs

--- src/main/java/network/palace/dashboard/commands/UpdateHashesCommand.java
+++ src/main/java/network/palace/dashboard/commands/UpdateHashesCommand.java
@@ -20,7 +20,9 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Marc on 3/26/17.
@@ -40,9 +42,25 @@ public class UpdateHashesCommand extends DashboardCommand {
                 HashMap<String, ResourcePack> list = new HashMap<>();
 
                 for (Document doc : dashboard.getMongoHandler().getResourcePackCollection().find()) {
-                    list.put(doc.getString("name"), new ResourcePack(doc.getString("name"),
-                            doc.getString("url"), doc.getString("hash"), false));
+                    String name = doc.getString("name");
+                    ResourcePack pack = new ResourcePack(name);
+                    List<ResourcePack.Version> versions = new ArrayList<>();
+
+                    if (doc.containsKey("versions")) {
+                        for (Object o : doc.get("versions", ArrayList.class)) {
+                            Document version = (Document) o;
+                            int protocolId = version.getInteger("id");
+
+                            versions.add(pack.generateVersion(protocolId, version.getString("url"), version.containsKey("hash") ? version.getString("hash") : ""));
+                        }
+                    } else {
+                        versions.add(pack.generateVersion(-1, doc.getString("url"), doc.containsKey("hash") ? doc.getString("hash") : ""));
+                    }
+                    pack.setVersions(versions);
+
+                    list.put(name, pack);
                 }
+
                 File dir = new File("packs");
                 if (!dir.exists()) {
                     dir.mkdir();
@@ -59,46 +77,63 @@ public class UpdateHashesCommand extends DashboardCommand {
                         }
                     }
                 }
+
                 for (ResourcePack pack : list.values()) {
-                    try {
-                        player.sendMessage(ChatColor.GREEN + "Downloading " + pack.getName() + " from " + pack.getUrl());
-                        String path = "packs/" + pack.getName() + System.currentTimeMillis();
-                        URL website = new URL(pack.getUrl());
-                        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-                        FileOutputStream fos = new FileOutputStream(path);
-                        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                        File f = new File(path);
-                        if (!f.exists()) {
-                            player.sendMessage(ChatColor.RED + "There was an error downloading " + pack.getName() + "!");
-                        } else {
-                            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-                            InputStream fis = new FileInputStream(f);
-                            int n = 0;
-                            byte[] buffer = new byte[8192];
-                            while (n != -1) {
-                                n = fis.read(buffer);
-                                if (n > 0) {
-                                    digest.update(buffer, 0, n);
+                    for (ResourcePack.Version version : pack.getVersions()) {
+                        try {
+                            player.sendMessage(ChatColor.GREEN + "Downloading " + version.getName() + " from " + version.getUrl());
+                            String path = "packs/" + version.getName() + System.currentTimeMillis();
+                            URL website = new URL(version.getUrl());
+                            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+                            FileOutputStream fos = new FileOutputStream(path);
+                            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+                            File f = new File(path);
+                            if (!f.exists()) {
+                                player.sendMessage(ChatColor.RED + "There was an error downloading " + version.getName() + "!");
+                            } else {
+                                MessageDigest digest = MessageDigest.getInstance("SHA-1");
+                                InputStream fis = new FileInputStream(f);
+                                int n = 0;
+                                byte[] buffer = new byte[8192];
+                                while (n != -1) {
+                                    n = fis.read(buffer);
+                                    if (n > 0) {
+                                        digest.update(buffer, 0, n);
+                                    }
                                 }
+                                String hash = DatatypeConverter.printHexBinary(digest.digest()).toLowerCase();
+                                if (!hash.equals(version.getHash())) {
+                                    version.setHash(hash);
+                                    version.setUpdated(true);
+                                }
+                                player.sendMessage(ChatColor.GREEN + "SHA-1 hash for " + version.getName() + " is " + hash);
                             }
-                            String hash = DatatypeConverter.printHexBinary(digest.digest()).toLowerCase();
-                            if (!hash.equals(pack.getHash())) {
-                                pack.setHash(hash);
-                                pack.setUpdated(true);
-                            }
-                            player.sendMessage(ChatColor.GREEN + "SHA-1 hash for " + pack.getName() + " is " + hash);
+                        } catch (IOException | NoSuchAlgorithmException e) {
+                            e.printStackTrace();
+                            player.sendMessage(ChatColor.RED + e.getClass().getName() + e.getMessage());
                         }
-                    } catch (IOException | NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                        player.sendMessage(ChatColor.RED + e.getClass().getName() + e.getMessage());
                     }
                 }
+
                 for (ResourcePack pack : list.values()) {
-                    if (!pack.isUpdated()) continue;
-                    dashboard.getMongoHandler().getResourcePackCollection().updateOne(Filters.eq("name",
-                            pack.getName()), Updates.set("hash", pack.getHash()));
-                    player.sendMessage(ChatColor.YELLOW + "Updated hash for " + pack.getName());
+                    for (ResourcePack.Version version : pack.getVersions()) {
+                        if (!version.isUpdated()) continue;
+                        if (version.getProtocolId() == -1) {
+                            dashboard.getMongoHandler().getResourcePackCollection().updateOne(Filters.eq("name",
+                                    pack.getName()), Updates.set("hash", version.getHash()));
+                        } else {
+                            dashboard.getMongoHandler().getResourcePackCollection()
+                                    .updateOne(Filters.and(
+                                            Filters.eq("name", pack.getName()),
+                                            Filters.elemMatch("versions",
+                                                    Filters.eq("id", version.getProtocolId()))
+                                            ),
+                                            Updates.set("versions.$.hash", version.getHash()));
+                        }
+                        player.sendMessage(ChatColor.YELLOW + "Updated hash for " + version.getName());
+                    }
                 }
+
                 player.sendMessage(ChatColor.GREEN + "Clearing download folder...");
                 for (File file : dir.listFiles()) {
                     if (!file.isDirectory()) {
@@ -113,11 +148,34 @@ public class UpdateHashesCommand extends DashboardCommand {
         });
     }
 
-    @AllArgsConstructor
+    @Getter
+    @Setter
     private class ResourcePack {
-        @Getter private String name;
-        @Getter private String url;
-        @Getter @Setter private String hash;
-        @Getter @Setter private boolean updated;
+        private String name;
+        private List<Version> versions = new ArrayList<>();
+
+        public ResourcePack(String name) {
+            this.name = name;
+        }
+
+        public Version generateVersion(int protocolId, String url, String hash) {
+            return new Version(protocolId, url, hash, false);
+        }
+
+        @Getter
+        @AllArgsConstructor
+        protected class Version {
+            private int protocolId; //This is the highest protocol id this pack works for
+            private String url;
+            @Setter private String hash;
+            @Setter private boolean updated;
+
+            public String getName() {
+                if (protocolId != -1) {
+                    return ResourcePack.this.name + "_" + protocolId;
+                }
+                return ResourcePack.this.name;
+            }
+        }
     }
 }
