15a4883d731436b06085bda7447b7369d4f090dc
Marc <legobuilder0813@gmail.com>
2019-10-15 04:30:48 -0400

    Add 2-second delay to /cc

--- src/main/java/network/palace/dashboard/commands/ClearChatCommand.java
+++ src/main/java/network/palace/dashboard/commands/ClearChatCommand.java
@@ -7,8 +7,11 @@ import network.palace.dashboard.handlers.DashboardCommand;
 import network.palace.dashboard.handlers.Player;
 import network.palace.dashboard.handlers.Rank;
 
+import java.util.HashMap;
+
 public class ClearChatCommand extends DashboardCommand {
     public String clearMessage = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    private HashMap<String, Long> lastCleared = new HashMap<>();
 
     public ClearChatCommand() {
         super(Rank.TRAINEE);
@@ -19,8 +22,26 @@ public class ClearChatCommand extends DashboardCommand {
         Dashboard dashboard = Launcher.getDashboard();
         String server = player.getServer();
         boolean park = dashboard.getServer(server).isPark();
+        if (System.currentTimeMillis() - (lastCleared.getOrDefault(park ? "ParkChat" : server, 0L)) < 2000) {
+            //if this chat was last cleared up to 2 seconds ago, prevent it from being cleared again
+            player.sendMessage(ChatColor.YELLOW + "It hasn't been 2 seconds since the last chat clear!");
+            return;
+        }
+        lastCleared.put(park ? "ParkChat" : server, System.currentTimeMillis());
+        HashMap<String, Boolean> parkChatCache = new HashMap<>();
         for (Player tp : dashboard.getOnlinePlayers()) {
-            if (tp.getServer().equals(server) || (park && dashboard.getServer(tp.getServer()).isPark())) {
+            boolean clear;
+            if (park) {
+                if (parkChatCache.containsKey(tp.getServer())) {
+                    clear = parkChatCache.get(tp.getServer());
+                } else {
+                    clear = dashboard.getServer(tp.getServer()).isPark();
+                    parkChatCache.put(tp.getServer(), clear);
+                }
+            } else {
+                clear = tp.getServer().equals(server);
+            }
+            if (clear) {
                 if (tp.getRank().getRankId() < Rank.TRAINEE.getRankId()) {
                     tp.sendMessage(clearMessage + ChatColor.DARK_AQUA + "Chat has been cleared");
                 } else {
