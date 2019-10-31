01aa82fab7a810b753a6c2d794bd5a970fbe80cc
Marc <legobuilder0813@gmail.com>
2019-10-31 15:47:11 -0400

    Fix /stafflist merging certain ranks

--- src/main/java/network/palace/dashboard/commands/StaffListCommand.java
+++ src/main/java/network/palace/dashboard/commands/StaffListCommand.java
@@ -18,7 +18,7 @@ public class StaffListCommand extends DashboardCommand {
 
     @Override
     public void execute(Player player, String label, String[] args) {
-        TreeMap<Rank, Set<Player>> players = new TreeMap<>((r1, r2) -> r2.getRankId() - r1.getRankId());
+        TreeMap<Rank, Set<Player>> players = new TreeMap<>((r1, r2) -> r2.ordinal() - r1.ordinal());
         Launcher.getDashboard().getOnlinePlayers().stream()
                 .filter(p -> p.getRank().getRankId() >= Rank.TRAINEE.getRankId())
                 .forEach(tp -> {
