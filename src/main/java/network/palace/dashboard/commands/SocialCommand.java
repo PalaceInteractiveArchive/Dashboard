b9701123721cfa176f6acff9550de3f691afc9f6
Marc <legobuilder0813@gmail.com>
2019-12-18 02:20:59 -0500

    Update links in /social

--- src/main/java/network/palace/dashboard/commands/SocialCommand.java
+++ src/main/java/network/palace/dashboard/commands/SocialCommand.java
@@ -10,17 +10,17 @@ public class SocialCommand extends DashboardCommand {
     @Override
     public void execute(Player player, String label, String[] args) {
         player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "Palace Network Social Links:");
-        player.send(new PacketLink(player.getUniqueId(), "https://palace.network", "Website: https://palace.network",
+        player.send(new PacketLink(player.getUniqueId(), "https://forums.palace.network", "Forums: https://forums.palace.network",
                 ChatColor.GREEN, false, false));
-        player.send(new PacketLink(player.getUniqueId(), "https://palace.network/discord",
-                "Discord: https://palace.network/Discord", ChatColor.YELLOW, false, false));
+        player.send(new PacketLink(player.getUniqueId(), "https://palnet.us/Discord",
+                "Discord: https://palnet.us/Discord", ChatColor.YELLOW, false, false));
         player.send(new PacketLink(player.getUniqueId(), "https://twitter.com/PalaceNetwork",
                 "Twitter: @PalaceNetwork", ChatColor.AQUA, false, false));
         player.send(new PacketLink(player.getUniqueId(), "https://youtube.com/MCMagicParks",
                 "YouTube: https://youtube.com/MCMagicParks", ChatColor.RED, false, false));
         player.send(new PacketLink(player.getUniqueId(), "https://facebook.com/PalaceNetworkMC",
                 "Facebook: https://facebook.com/PalaceNetworkMC", ChatColor.BLUE, false, false));
-        player.send(new PacketLink(player.getUniqueId(), "https://instagram.com/PalaceNetwork",
-                "Instagram: https://instagram.com/PalaceNetwork", ChatColor.LIGHT_PURPLE, false, false));
+        player.send(new PacketLink(player.getUniqueId(), "https://instagram.com/palacenetwork",
+                "Instagram: https://instagram.com/palacenetwork", ChatColor.LIGHT_PURPLE, false, false));
     }
 }
