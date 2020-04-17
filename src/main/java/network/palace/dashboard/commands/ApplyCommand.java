57f76762aab973fb4ef53bfb6f8e0b71505612ab
Vidsify <vidsify@hotmail.com>
2020-04-17 19:44:06 +0100

    Added /apply command

--- src/main/java/network/palace/dashboard/commands/ApplyCommand.java
+++ src/main/java/network/palace/dashboard/commands/ApplyCommand.java
@@ -1,4 +1,16 @@
 package network.palace.dashboard.commands;
 
-public class ApplyCommand {
+import network.palace.dashboard.chat.ChatColor;
+import network.palace.dashboard.handlers.DashboardCommand;
+import network.palace.dashboard.handlers.Player;
+import network.palace.dashboard.packets.dashboard.PacketLink;
+
+public class ApplyCommand extends DashboardCommand {
+
+    @Override
+    public void execute(Player player, String label, String[] args) {
+        PacketLink packet = new PacketLink(player.getUniqueId(), "https://palnet.us/apply", "Click here to apply",
+                ChatColor.YELLOW, true);
+        player.send(packet);
+    }
 }
