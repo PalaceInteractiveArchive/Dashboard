41d5022dad821870bce59551e73fdd08193273d8
Parker Smith <parker@yeetdev.com>
2020-04-18 12:13:20 -0700

    Showing Dan how to use ComponentBuilder for hover

--- src/main/java/network/palace/dashboard/commands/ApplyCommand.java
+++ src/main/java/network/palace/dashboard/commands/ApplyCommand.java
@@ -11,6 +11,6 @@ public class ApplyCommand extends DashboardCommand {
         player.sendMessage(new ComponentBuilder("\nClick to see what positions we have available!\n")
         .color(ChatColor.YELLOW).underlined(false).bold(true)
         .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://palnet.us/apply"))
-        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, "Click me")).create());
+        .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to visit https://palnet.us/apply").color(ChatColor.GREEN).create())).create());
     }
 }
