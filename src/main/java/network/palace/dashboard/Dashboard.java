9d054c0e4d68cf7be9f8036497b2c7bbf18e7fcd
Marc DiFilippo <legobuilder0813@gmail.com>
2020-07-07 21:55:57 -0400

    Update maintenance whitelist

--- src/main/java/network/palace/dashboard/Dashboard.java
+++ src/main/java/network/palace/dashboard/Dashboard.java
@@ -109,8 +109,9 @@ public class Dashboard {
         if (maintenance) {
             maintenanceWhitelist.clear();
             System.out.println("Loading list of staff members for maintenance mode...");
-            List<UUID> staff = mongoHandler.getPlayersByRank(Rank.TRAINEE, Rank.TRAINEEBUILD, Rank.MOD, Rank.BUILDER,
-                    Rank.ARCHITECT, Rank.COORDINATOR, Rank.DEVELOPER, Rank.LEAD, Rank.MANAGER, Rank.DIRECTOR);
+            List<UUID> staff = mongoHandler.getPlayersByRank(Rank.TRAINEE, Rank.TRAINEEBUILD, Rank.TRAINEETECH, Rank.MOD,
+                    Rank.MEDIA, Rank.TECHNICIAN, Rank.BUILDER, Rank.ARCHITECT, Rank.COORDINATOR, Rank.DEVELOPER, Rank.LEAD,
+                    Rank.MANAGER, Rank.DIRECTOR, Rank.OWNER);
             maintenanceWhitelist.addAll(staff);
             System.out.println("Finished loading staff member list for maintenance mode!");
         }
