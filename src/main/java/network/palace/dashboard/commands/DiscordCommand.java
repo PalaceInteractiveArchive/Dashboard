53562db0b4039e7e122c5547cf7b3d387765ed60
Flakenstine <isaac@palace.interactive>
2021-01-22 16:41:55 -0700

    Made changes to DiscordCommand.java Added link argument to command

--- src/main/java/network/palace/dashboard/commands/DiscordCommand.java
+++ src/main/java/network/palace/dashboard/commands/DiscordCommand.java
@@ -1,7 +1,11 @@
 package network.palace.dashboard.commands;
 
+import io.socket.parser.Packet;
 import network.palace.dashboard.Launcher;
 import network.palace.dashboard.chat.ChatColor;
+import network.palace.dashboard.chat.ClickEvent;
+import network.palace.dashboard.chat.ComponentBuilder;
+import network.palace.dashboard.chat.HoverEvent;
 import network.palace.dashboard.discordSocket.DiscordUserInfo;
 import network.palace.dashboard.handlers.DashboardCommand;
 import network.palace.dashboard.handlers.Player;
@@ -19,48 +23,18 @@ public class DiscordCommand extends DashboardCommand {
     public void execute(Player player, String label, String[] args) {
         if (args.length == 0 ) {
             boolean isLinked = Launcher.getDashboard().getMongoHandler().verifyDiscordLink(player.getUuid());
-            if(!isLinked) {
-                PacketLink packet = new PacketLink(player.getUniqueId(), "https://palnet.us/Discord",
-                        "Click for more information about Discord!", ChatColor.YELLOW, true);
+            if (isLinked) {
+                player.sendMessage(ChatColor.GREEN + "Hey! " + player.getUsername() + " you have a linked discord account with the username: " + ChatColor.YELLOW + ChatColor.BOLD + " JohnSmith#0000");
             } else {
-                player.sendMessage(ChatColor.GREEN + "Hey! " + player.getUsername() + " you have a linked discord account with the username " + ChatColor.YELLOW + ChatColor.BOLD + "JohnSmith#123456");
+                player.sendMessage(ChatColor.GREEN + "Hey! " + player.getUsername() + " you haven't yet linked your discord account with our server. Please run: " +
+                        ChatColor.YELLOW + ChatColor.BOLD + "/discord link");
             }
+        } else if (args.length >= 2 && args[0].equals("link")) {
+            player.sendMessage(new ComponentBuilder("\nClick to link your discord account\n")
+            .color(ChatColor.YELLOW).underlined(false).bold(true)
+            .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "27\n" +
+                    "https://discord.com/api/oauth2/authorize?response_type=code&client_id=543141358496383048&scope=identify&state=" + player.getUuid().toString() + "&redirect_uri=https://internal-api.palace.network/"))
+            .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Opens a browser window to start the discord linking process").color(ChatColor.GREEN).create())).create());
         }
-//        if (args.length <2) {
-//            PacketLink packet = new PacketLink(player.getUniqueId(), "https://palnet.us/Discord",
-//                    "Click for more information about Discord!", ChatColor.YELLOW, true);
-//            player.send(packet);
-//        } else if (args.length >= 2 && args[0].equals("link")) {
-//            int discordPin = DiscordUtil.generatePin(player, 10000, 99999);
-//
-//            //this is for testing purposes. We will store this info in mongo.
-//            player.sendMessage(ChatColor.GREEN + "Hey! " + player.getUsername() + " your unique discord pin is " + ChatColor.YELLOW + ChatColor.BOLD + discordPin);
-//        }
     }
-
-//    @Override
-//    public void execute(Player player, String label, String[] args) {
-//        if (args.length < 2) {
-//            PacketLink packet = new PacketLink(player.getUniqueId(), "https://palnet.us/Discord",
-//                    "Click for more information about Discord!", ChatColor.YELLOW, true);
-//            player.send(packet);
-//        } else if (args.length >= 2 && args[0].equals("link")) {
-//            StringBuilder fullName = new StringBuilder();
-//            for (int i = 1; i < args.length; i++) {
-//                fullName.append(args[i]);
-//            }
-//            if (fullName.toString().contains("#") && fullName.toString().matches("(.*)#(\\d+)")) {
-//                DiscordUserInfo userInfo = new DiscordUserInfo(fullName.toString(), player.getUsername(), player.getUniqueId().toString(), player.getRank().toString());
-//                if (SocketConnection.sendLink(userInfo)) {
-//                    player.sendMessage(ChatColor.GREEN + "");
-//                }
-//            } else {
-//                player.sendMessage(ChatColor.DARK_RED + "Please specify a valid Discord ID!");
-//            }
-//        } else {
-//            PacketLink packet = new PacketLink(player.getUniqueId(), "https://palnet.us/Discord",
-//                    "Click for more information about Discord!", ChatColor.YELLOW, true);
-//            player.send(packet);
-//        }
-//    }
 }
